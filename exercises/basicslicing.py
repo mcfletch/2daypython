01c1ed3e73414ee6b878dc88bf328d0c59cebd5d
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-02-22 16:25:34 -0500

    Remove the unused basic scripts...

--- exercises/basicslicing.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# basicslicing.py
-
-counts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print 'counts', counts
-print 'counts[1:]', counts[1:]
-print 'counts[-1:]', counts[-1:]
-print 'counts[1:-1]', counts[1:-1]
-print 'counts[99:199]', counts[99:199]      # not an error
