8e0ce4c44a7f05208496f5f56a8b675a38653a89
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-02-21 21:51:41 -0500

    Remove unused "get" operations from the documentation...

--- exercises/dictdefinitions.py
+++ exercises/dictdefinitions.py
@@ -6,9 +6,6 @@ dictionary['this'] = 'that'
 dictionary['those'] = 23
 assert dictionary['those'] == 23
 del dictionary['those']
-dictionary.get( 'those' ) # returns None 
-dictionary.get( 'those', 3 ) # returns 3
-dictionary.get( 'this' ) # returns 'that'
 
 print dictionary.items() # [('this','that')]
 print dictionary.values() # ['that']
