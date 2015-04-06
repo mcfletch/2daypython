28255c4b62644f6e7355ad0be51be247754b994b
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-04-06 02:37:39 -0400

    Simplify the game, but it's still too big a leap from guessing game

--- exercises/tictactoe/game.py
+++ exercises/tictactoe/game.py
@@ -1,10 +1,22 @@
-SIDES = ['X', 'O']
+SIDES = ['X', 'Y']
+board = [None]*9
+WINNING_LINES = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6], 
+]
 
 def print_board( board ):
     result = []
-    for i, row in enumerate(board):
+    for i in range(3):
         row_description = []
-        for j, value in enumerate(row):
+        for j in range(3):
+            value = board[j+i*3]
             if value is None:
                 row_description.append(' %s '%(j+i*3))
             else:
@@ -12,19 +24,6 @@ def print_board( board ):
         row_description = ' | '.join( row_description )
         result.append( row_description )
     return "\n".join(result)
-
-def rows(board):
-    for row in board:
-        yield row 
-def cols(board):
-    for i in range(3):
-        yield [row[i] for row in board]
-def diagonals(board):
-    for indices in [
-        [(0, 0), (1, 1), (2, 2)], 
-        [(2, 0), (1, 1), (0, 2)], 
-    ]:
-        yield [ board[y][x] for (x, y) in indices ]
 
 def all_one( line ):
     current = None
@@ -38,47 +37,28 @@ def all_one( line ):
     return True
 
 def finished( board ):
-    for row in rows(board):
-        if all_one(row):
-            return True 
-    for col in cols(board):
-        if all_one(col):
-            return True
-    for diag in diagonals(board):
-        if all_one(diag):
+    for set in WINNING_LINES:
+        if all_one([board[i] for i in set]):
             return True
     return False
 
-board = [
-    [None, None, None], 
-    [None, None, None], 
-    [None, None, None], 
-]
-
-
 def read_coordinate( board, side ):
     while True:
-        move = raw_input( "%s's Move x,y: "%(SIDES[side], ) )
+        move = raw_input( "%s's Move: "%(SIDES[side], ) )
         move = int(move)
-        x = move % 3
-        y = move // 3
-        if board[y][x] is None:
-            return x,y
+        if board[move] is None:
+            return move
         else:
-            print( "That index is assigned: %s, %s"%(x, y))
+            print( "That index is assigned: %s"%(move, ))
 
 def play_game(board):
     current_move = 0
     while not finished(board):
         print print_board(board)
-        x, y = read_coordinate( board, current_move )
-        board[y][x] = current_move
+        move = read_coordinate( board, current_move )
+        board[move] = current_move
         current_move = (current_move + 1) %2
     print print_board(board)
-
-assert all_one([1, 1, 1])
-assert all_one([0, 0, 0])
-assert list(diagonals( [[0, 1, 2], [3, 4, 5], [6, 7, 8]])) == [[0, 4, 8], [2, 4, 6]]
 
 if __name__ == "__main__":
     play_game(board)
