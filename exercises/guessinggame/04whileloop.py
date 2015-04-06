e6eac17101f942c2c4c5c8a25b3ba86161dc3479
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-04-05 22:43:43 -0400

    Use a more idiomatic while loop in the example code

--- exercises/guessinggame/04whileloop.py
+++ exercises/guessinggame/04whileloop.py
@@ -1,14 +1,14 @@
 import random
 number = random.randint( 1, 100 )
 
-while True:
+guess = 0
+while guess != number:
 
     guess = raw_input('What is your guess? ')
     guess = int(guess)
 
     if guess == number:
         print( "Yay!")
-        break
     elif guess > number:
         print( "Too high")
     else:
